69aca03f735f6c84eab4bbf0a084995f78010470
rajat mishra <rajat.mishra@tntra.io>
2021-12-17 12:38:43 +0530

    djangoreactfullstack

---
+++ blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello from fullstack develoment using django and react !! blog by rajat and mittu thakkar </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
